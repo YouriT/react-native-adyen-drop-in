31bfc81ee9cf69732e5d7d480efb09b8546c74b9
Youri Tolstoy <ytolstoy@gmail.com>
2020-01-02 15:29:45 +0100

    progress

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
-import { NativeEventEmitter, NativeModules } from "react-native";
+import { NativeEventEmitter, NativeModules } from 'react-native';
 
 const AdyenDropIn = NativeModules.AdyenDropInPayment;
 const EventEmitter = new NativeEventEmitter(AdyenDropIn);
 const eventMap = {};
-let onPaymentProvideListener;
-let onPaymentFailListener;
-let onPaymentSubmitListener;
-let onPaymentCancelListener;
 const addListener = (key, listener) => {
-  if (eventMap[key]) {
-    return eventMap[key];
+  if (!eventMap[key]) {
+    eventMap[key] = [];
   }
-  eventMap[key] = listener;
-  EventEmitter.addListener(key, listener);
+  const ee = EventEmitter.addListener(key, listener);
+  eventMap[key].push(ee);
+  return ee;
 };
+
 export default {
   /**
    * Starting payment process.
@@ -30,12 +28,21 @@ export default {
    * @returns {*}
    */
   paymentMethods(paymentMethodJson) {
-    if (typeof paymentMethodJson === "object") {
+    if (typeof paymentMethodJson === 'object') {
       paymentMethodJson = JSON.stringify(paymentMethodJson);
     }
-    this._validateParam(paymentMethodJson, "paymentMethods", "string");
+    this._validateParam(paymentMethodJson, 'paymentMethods', 'string');
     return AdyenDropIn.paymentMethods(paymentMethodJson);
   },
+
+  /**
+   * Popup card payment form
+   * @param {*} paymentMethodJson
+   * @param {*} name
+   * @param {*} showHolderField show holder name field
+   * @param {*} showStoreField show store card details toggle
+   * @param {*} buttonTitle content of the submit button
+   */
   cardPaymentMethod(
     paymentMethodJson,
     name,
@@ -43,13 +50,13 @@ export default {
     showStoreField,
     buttonTitle
   ) {
-    if (typeof paymentMethodJson === "object") {
+    if (typeof paymentMethodJson === 'object') {
       paymentMethodJson = JSON.stringify(paymentMethodJson);
     }
-    this._validateParam(paymentMethodJson, "cardPaymentMethod", "string");
+    this._validateParam(paymentMethodJson, 'cardPaymentMethod', 'string');
     showHolderField = showHolderField || false;
     showStoreField = showStoreField || false;
-    buttonTitle = buttonTitle || "";
+    buttonTitle = buttonTitle || '';
     return AdyenDropIn.cardPaymentMethod(
       paymentMethodJson,
       name,
@@ -65,18 +72,18 @@ export default {
    * @returns {*}
    */
   storedCardPaymentMethod(paymentMethodJson, index) {
-    if (typeof paymentMethodJson === "object") {
+    if (typeof paymentMethodJson === 'object') {
       paymentMethodJson = JSON.stringify(paymentMethodJson);
     }
-    this._validateParam(paymentMethodJson, "storedCardPaymentMethod", "string");
+    this._validateParam(paymentMethodJson, 'storedCardPaymentMethod', 'string');
     index = index || 0;
     return AdyenDropIn.storedCardPaymentMethod(paymentMethodJson, index);
   },
   contractPaymentMethod(paymentMethodJson, index) {
-    if (typeof paymentMethodJson === "object") {
+    if (typeof paymentMethodJson === 'object') {
       paymentMethodJson = JSON.stringify(paymentMethodJson);
     }
-    this._validateParam(paymentMethodJson, "contractPaymentMethod", "string");
+    this._validateParam(paymentMethodJson, 'contractPaymentMethod', 'string');
     index = index || 0;
     return AdyenDropIn.contractPaymentMethod(paymentMethodJson, index);
   },
@@ -86,17 +93,17 @@ export default {
    * @returns {*}
    */
   handleAction(actionJson) {
-    if (typeof actionJson === "object") {
+    if (typeof actionJson === 'object') {
       actionJson = JSON.stringify(actionJson);
     }
-    this._validateParam(actionJson, "handleAction", "string");
+    this._validateParam(actionJson, 'handleAction', 'string');
     return AdyenDropIn.handleAction(actionJson);
   },
   handlePaymentResult(paymentJson) {
-    if (typeof paymentJson === "object") {
+    if (typeof paymentJson === 'object') {
       paymentJson = JSON.stringify(paymentJson);
     }
-    this._validateParam(paymentJson, "handlePaymentResult", "string");
+    this._validateParam(paymentJson, 'handlePaymentResult', 'string');
     return AdyenDropIn.handlePaymentResult(paymentJson);
   },
   encryptCard(cardNumber, expiryMonth, expiryYear, securityCode) {
@@ -111,8 +118,8 @@ export default {
    *  call when need to do more action
    */
   onPaymentProvide(mOnPaymentProvide) {
-    this._validateParam(mOnPaymentProvide, "onPaymentProvide", "function");
-    onPaymentProvideListener = addListener("onPaymentProvide", e => {
+    this._validateParam(mOnPaymentProvide, 'onPaymentProvide', 'function');
+    return addListener('onPaymentProvide', e => {
       mOnPaymentProvide(e);
     });
   },
@@ -138,11 +145,8 @@ export default {
    * @param {mOnError} mOnError
    */
   onPaymentFail(mOnPaymentFail) {
-    this._validateParam(mOnPaymentFail, "onPaymentFail", "function");
-    if (onPaymentFailListener) {
-      EventEmitter.removeListener(onPaymentFailListener);
-    }
-    onPaymentFailListener = addListener("onPaymentFail", e => {
+    this._validateParam(mOnPaymentFail, 'onPaymentFail', 'function');
+    return addListener('onPaymentFail', e => {
       mOnPaymentFail(e);
     });
   },
@@ -150,11 +154,8 @@ export default {
    * call when payment submit ,send to server do payments
    */
   onPaymentSubmit(mOnPaymentSubmit) {
-    this._validateParam(mOnPaymentSubmit, "onPaymentSubmit", "function");
-    if (onPaymentSubmitListener) {
-      EventEmitter.removeListener(onPaymentSubmitListener);
-    }
-    onPaymentSubmitListener = addListener("onPaymentSubmit", e => {
+    this._validateParam(mOnPaymentSubmit, 'onPaymentSubmit', 'function');
+    return addListener('onPaymentSubmit', e => {
       mOnPaymentSubmit(e);
     });
   },
@@ -169,24 +170,24 @@ export default {
     if (typeof param !== requiredType) {
       throw new Error(
         `Error: AdyenDropIn.${methodName}() requires a ${
-          requiredType === "function" ? "callback function" : requiredType
+          requiredType === 'function' ? 'callback function' : requiredType
         } but got a ${typeof param}`
       );
     }
   },
   events: EventEmitter,
   removeListeners() {
-    if (onPaymentProvideListener) {
-      EventEmitter.removeListener(onPaymentProvideListener);
-    }
-    if (onPaymentFailListener) {
-      EventEmitter.removeListener(onPaymentFailListener);
-    }
-    if (onPaymentSubmitListener) {
-      EventEmitter.removeListener(onPaymentSubmitListener);
-    }
-    if (onPaymentCancelListener) {
-      EventEmitter.removeListener(onPaymentCancelListener);
-    }
+    Object.keys(eventMap).forEach(eventKey => {
+      if (!eventMap[eventKey]) {
+        return;
+      }
+      eventMap[eventKey].forEach(ee => {
+        if (!ee.remove) {
+          return;
+        }
+        ee.remove();
+      });
+      eventMap[eventKey] = undefined;
+    });
   }
 };
